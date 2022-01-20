9d1a2067a9576c66c4254420582969a7f34466cb
SabariKrishnanB <98115764+SabariKrishnanB@users.noreply.github.com>
2022-01-21 00:31:06 +0530

    Update FavoriteUsers.js

--- src/actions/FavoriteUsers.js
+++ src/actions/FavoriteUsers.js
@@ -7,48 +7,44 @@ import { fetchData, ManageFav,DeleteFav } from './actions';
  class FavoriteUsers extends React.Component {
      onClickDelete=(id)=>{
       this.props.DeleteFav(id, this.props.data)
-     }
+    }
    render() {
-       console.log('ss');
          return ( 
-<div className="App">
-  <div className='container'>
-    <div>
-    <h1>Fav Users</h1>
+                  <div className="App">
+                  <div className='container'>
+                  <div>
+                      <h1>Fav Users</h1>
     
-    </div>
+                  </div>
    
-         { (this.props.data.data.filter(item => item.fav)).map(item => 
+                { (this.props.data.data.filter(item => item.fav)).map(item => 
         
-          <div key={item.id} className='detail'>
-            <div className='cover'>
-           <p> <img src={item.avatar} alt={item.first_name} /></p>
-           <p class="namebold">{item.first_name} {item.last_name}</p><br></br>
-           </div>
-            <p>{item.first_name}</p>
-            <p>{item.last_name}</p>
-            <p>{item.email}</p>
-             <button class="FavButton" onClick={()=>{this.onClickDelete(item.id)}}>
-                Delete from Favorite
-              </button>
-              
-        
-        
-        </div>)}
-     </div>   
-     </div>
-     );
-   }
- }
+                    <div key={item.id} className='detail'>
+                      <div className='cover'>
+                        <p> <img src={item.avatar} alt={item.first_name} /></p>
+                        <p class="namebold">{item.first_name} {item.last_name}</p><br></br>
+                      </div>
+                    <p>{item.first_name}</p>
+                    <p>{item.last_name}</p>
+                    <p>{item.email}</p>
+                    <button class="FavButton" onClick={()=>{this.onClickDelete(item.id)}}>
+                      Delete from Favorite
+                    </button>
+                    </div>)}
+               </div>   
+            </div>
+          );
+        }
+     }
 
 const mapStatetoProps = (state) => {
   return {  data: state.data }
-}
+  }
 
 const mapDispatchprops = (dispatch) => {
   return { 
             DeleteFav: (id, fetchData) => dispatch(ManageFav(id, fetchData)) }
-}
+  }
 
 
 
