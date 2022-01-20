5c2036a1bd816e6eb159432215bf83dfefad4f8f
SabariKrishnanB <98115764+SabariKrishnanB@users.noreply.github.com>
2022-01-21 00:32:37 +0530

    Update actions.js

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -5,12 +5,10 @@ export const ManageFav = (id, oldData) => {
     const data = oldData.data.map(item => {
         return (item.id === id ? {...item, fav:!item.fav}: item)
     })
-    
-    console.log('===', oldData, data)
      return {
          type: "ManageFav",
          data: {...oldData,data:data}
-    }
+            }
 }
 
 
@@ -19,7 +17,6 @@ export const SearchTerm = (text, oldData) => {
     const data = oldData.data.map(item => {
         return (item.first_name.toLowerCase().includes(text) ? {...item, visible:true}: {...item, visible:false})
     })
-    console.log('===', oldData, data)
      return {
          type: "SearchTerm",
          data: {...oldData,data:data}
@@ -32,24 +29,20 @@ export const DeleteFav = (id, oldData) => {
         return (item.id === id ? {...item, fav:false}: item)
 
     })
-    
-    console.log('===', oldData, data)
      return {
          type: "DeleteFav",
          data: {...oldData,data:data}
-    }
+            }
 }
 
 // asynchronous action creator
 export const fetchData = () => {
-
     return (dispatch) => {
         return fetch('https://reqres.in/api/users?page=${page}&per_page=${countPerPage}&delay=1`')
             .then(response => response.json())
             .then(json => {
                 const data = json.data.map(item => {
                     return {...item, visible:true, fav: false}
-               
                 })
                 return  dispatch({ type: "FetchData", data: {json, data:data} })
             })
